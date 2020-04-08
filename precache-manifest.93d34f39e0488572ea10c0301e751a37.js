030fda2b7f777cf7d9870b1fc8b1f254fc26f53a
Tom VanAntwerp <tom@tomvanantwerp.com>
2020-04-08 17:14:16 -0400

    Updates

---
+++ precache-manifest.93d34f39e0488572ea10c0301e751a37.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f521f525c0984b4fb4e35116b415418e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dcf68a721a955a798cce",
+    "url": "/static/js/2.184d491c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.184d491c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d766b4753ce8f7b538d",
+    "url": "/static/js/main.2df07ddc.chunk.js"
+  },
+  {
+    "revision": "aea40a29d51e57d8a0f3",
+    "url": "/static/js/runtime-main.2ffb88e4.js"
+  }
+]);
