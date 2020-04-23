48a30a38e8ea1bbe848ee12140b12b57cf47e925
Tom VanAntwerp <tom@tomvanantwerp.com>
2020-04-23 11:03:07 -0400

    Animate in label buttons

--- src/components/LabelButton.tsx
+++ src/components/LabelButton.tsx
@@ -1,18 +1,25 @@
+import React from 'react';
 import styled, { DefaultTheme } from 'styled-components';
+import { motion } from 'framer-motion';
 
-interface ILabelButton {
+interface IStyledLabelButton {
   selected: boolean;
   theme: DefaultTheme;
 }
 
-const LabelButton = styled.label`
-  background-color: ${({ selected, theme }: ILabelButton) =>
+interface ILabelButton {
+  selected: boolean;
+  htmlFor?: string;
+}
+
+const StyledLabelButton = styled(motion.label)`
+  background-color: ${({ selected, theme }: IStyledLabelButton) =>
     selected ? theme.teaGreen : theme.lightGreen};
   border: 1px solid
-    ${({ selected, theme }: ILabelButton) =>
+    ${({ selected, theme }: IStyledLabelButton) =>
       selected ? theme.teaGreen : theme.lightGreen};
   border-radius: 4px;
-  color: ${({ selected, theme }: ILabelButton) => theme.darkBrown};
+  color: ${({ theme }: IStyledLabelButton) => theme.darkBrown};
   cursor: pointer;
   display: block;
   padding: 5px 0;
@@ -22,5 +29,15 @@ const LabelButton = styled.label`
     display: none;
   }
 `;
+
+const LabelButton: React.FC<ILabelButton> = ({ selected, children }) => (
+  <StyledLabelButton
+    selected={selected}
+    initial={{ scale: 0 }}
+    animate={{ scale: 1 }}
+  >
+    {children}
+  </StyledLabelButton>
+);
 
 export default LabelButton;
