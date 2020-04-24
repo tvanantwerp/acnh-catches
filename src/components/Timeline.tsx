ce4ba00e2bbad0bfa7ab03d72cf8ab369e300cc9
Tom VanAntwerp <tom@tomvanantwerp.com>
2020-04-24 17:28:52 -0400

    Convert timelines to use canvas for performance reasons

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import styled, { DefaultTheme } from 'styled-components';
+import React, { useEffect, useRef } from 'react';
+
+import { Theme } from '../Theme';
 
 interface ITimes {
   theCatch: string;
@@ -7,44 +8,37 @@ interface ITimes {
   currentTime: number;
 }
 
-interface IHour {
-  theme: DefaultTheme;
-  yes: boolean;
-  currentTime: boolean;
-}
-
-const TimesContainer = styled.div`
-  display: flex;
-  height: 25px;
-  min-width: 100px;
-`;
-
-const StyledTime = styled.div`
-  background-color: ${({ theme, yes, currentTime }: IHour) =>
-    yes
-      ? currentTime
-        ? '#9cd08f'
-        : theme.teaGreen
-      : currentTime
-      ? '#cccccc'
-      : '#dedede'};
-  flex: 1;
-`;
-
 const Times = ({ theCatch, times, currentTime }: ITimes) => {
-  return (
-    <TimesContainer>
-      {times.split('').map((time, i) => {
-        return (
-          <StyledTime
-            key={`time-${theCatch}-${time}-${i}`}
-            yes={time === 'y'}
-            currentTime={currentTime === i}
-          />
-        );
-      })}
-    </TimesContainer>
-  );
+  const ref = useRef<HTMLCanvasElement>(null);
+  const height = 60;
+  const width = 240;
+
+  useEffect(() => {
+    if (ref && ref.current) {
+      const canvas: HTMLCanvasElement = ref.current;
+      canvas.width = width;
+      canvas.height = height;
+      const ctx = canvas.getContext('2d');
+      const count = times.length;
+
+      if (ctx) {
+        times.split('').forEach((time, i) => {
+          const color =
+            time === 'y'
+              ? i === currentTime
+                ? '#9cd08f'
+                : Theme.teaGreen
+              : i === currentTime
+              ? '#cccccc'
+              : '#dedede';
+          ctx.fillStyle = color;
+          ctx.fillRect(i * (width / count), 0, width / count, height);
+        });
+      }
+    }
+  }, [currentTime, ref, times]);
+
+  return <canvas style={{ height: '25px', width: '100%' }} ref={ref} />;
 };
 
 export default Times;
